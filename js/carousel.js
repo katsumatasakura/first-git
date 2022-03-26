d9e1d154110c14e347661f0aef7a58f97a4c6abb
sakura.katsumata <03sa26kura@gmail.com>
2022-03-26 13:55:19 +0000

    sp carousel pc grid

--- js/carousel.js
+++ js/carousel.js
@@ -1,3 +1,26 @@
-$(document).ready(function(){
-    $('.carousel').slick();
+// カルーセル
+$(function() {
+    function sliderSetting() {
+
+        var width = $(window).width();
+
+        if (width <= 768) {
+            $('.grid-layout').not('.slick-initialized').slick({
+                autoplay: false,
+                autoplaySpeed: 10000,
+                arrows: true,
+                dots: true,
+                fade: true,
+            });
+        }
+        else {
+            $('.slide.slick-initialized').slick('unslick');
+        }
+    }
+
+    sliderSetting();
+
+    $(window).resize(function() {
+        sliderSetting();
+    });
 });
