190c20a27442ecb3625f64a46adf2e97245173c3
sakura.katsumata <03sa26kura@gmail.com>
2022-03-26 11:13:15 +0000

    carouselうまくいかず

--- js/carousel.js
+++ js/carousel.js
@@ -1,24 +1,3 @@
-$(function() {
-    function sliderSetting() {
-
-        var width = $(window).width();
-
-        if (width <= 768) {
-            $('.carousel').not('.slick-initialized').slick({
-                autoplay: false,
-                arrows: true,
-                dots: true,
-                fade: false,
-            });
-        }
-        else {
-            $('.slide.slick-initialized').slick('unslick');
-        }
-    }
-
-    sliderSetting();
-
-    $(window).resize(function() {
-        sliderSetting();
-    });
+$(document).ready(function(){
+    $('.carousel').slick();
 });
